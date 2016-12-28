b4430f6feeb78f4f5021e04bbdeee4faeb00286f
Kifah Seif <kifahhk@yahoo.com>
2016-12-28 20:11:17 +0100

    modify folder structure and configuration for performing tests

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
+import {isDev, isTest, isProd} from './server';
+
+const devDB = 'mongodb://localhost:27017/nerm-db-dev';
+const testDB = 'mongodb://localhost:27017/nerm-db-test';
+const prodDB = 'mongodb://localhost:27017/nerm-db-prod';
 const config = {
-  mongoURL: 'mongodb://localhost:27017/nerm-db' || process.env.MONGO_URL,
-  port: process.env.PORT || 8000,
+  mongoURL: isDev ? devDB : isTest ? testDB : isProd ? prodDB : process.env.MONGO_URL,
+  port: isDev ? 8000 : isTest ? 8080 : isProd ? 23 : process.env.PORT,
 };
 
 export default config;
